0dce86833372c8f09aa3c3123c9ee3882f1f491c
Campalo <25295489+Campalo@users.noreply.github.com>
2019-04-19 22:31:33 +0200

    Updates

---
+++ precache-manifest.5900877fe9a2a622468ced754f46bd65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfaf65ab3bba91814399",
+    "url": "/ContentManager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "dfaf65ab3bba91814399",
+    "url": "/ContentManager/static/js/main.6ceec5af.chunk.js"
+  },
+  {
+    "revision": "f64a867b48a8cddb0c6a",
+    "url": "/ContentManager/static/js/runtime~main.835037e2.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ContentManager/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "319045087e4abaf974540c0029983f2a",
+    "url": "/ContentManager/index.html"
+  }
+];
