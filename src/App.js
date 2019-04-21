310e13ee35a6e75324144c2522a994a690aebc6f
Campalo <25295489+Campalo@users.noreply.github.com>
2019-04-21 21:43:44 +0200

    update App.js with links to new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>The App Component</h1>
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Karen Mill"
+            email="kmill@gmail.com"
+            phone="444-555-5555"
+          />
+        </div>
       </div>
     );
   }
