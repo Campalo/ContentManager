fcedfe2d0462f3543b4ba418ba5885d91a9225b5
Campalo <25295489+Campalo@users.noreply.github.com>
2019-05-05 20:04:11 +0200

    add the Provider component to App.js and the Consumer component to Contacts.js to wrap all contact info management

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,33 +1,8 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
+import { Consumer } from "../contextApi";
 
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "Jon Doe",
-          email: "jdoe@gmail.com",
-          phone: "555-555-5555"
-        },
-        {
-          id: 2,
-          name: "Karen Mill",
-          email: "kMill@gmail.com",
-          phone: "555-444-5555"
-        },
-        {
-          id: 3,
-          name: "Henry Johnson",
-          email: "henryj@gmail.com",
-          phone: "333-555-5555"
-        }
-      ]
-    };
-  }
-
   deleteContact = id => {
     const { contacts } = this.state;
     const newContacts = contacts.filter(contact => contact.id !== id);
@@ -38,18 +13,23 @@ class Contacts extends Component {
   };
 
   render() {
-    const { contacts } = this.state; //using DESTRUCTURING
     return (
-      // use React.Fragment to delete the global div wrppping Contacts because we don't need it
-      <React.Fragment>
-        {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            contact={contact}
-            deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-          />
-        ))}
-      </React.Fragment>
+      <Consumer>
+        {value => {
+          const { contacts } = value;
+          return (
+            <React.Fragment>
+              {contacts.map(contact => (
+                <Contact
+                  key={contact.id}
+                  contact={contact}
+                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
+                />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
